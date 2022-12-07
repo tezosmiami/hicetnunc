473474f9c2e1a862479fb698eea60214f8343127
tezosmiami <tezosmiami@gmail.com>
2022-12-06 21:19:21 -0500

    stun/turn. . .

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -127,6 +127,26 @@ export const Chat = () => {
           secure: true,
           debug: 3,
           path: "/hicetnunc",
+          config: {'iceServers': [
+            {
+              urls: "stun:openrelay.metered.ca:80",
+            },
+            {
+              urls: "turn:openrelay.metered.ca:80",
+              username: "openrelayproject",
+              credential: "openrelayproject",
+            },
+            {
+              urls: "turn:openrelay.metered.ca:443",
+              username: "openrelayproject",
+              credential: "openrelayproject",
+            },
+            {
+              urls: "turn:openrelay.metered.ca:443?transport=tcp",
+              username: "openrelayproject",
+              credential: "openrelayproject",
+            },
+          ]} 
         })
         peer.current.on('open', (id) =>{
           console.log('id: ', id)
