41e8d97eadf48004f5338713b03cc6d4356a750d
tezosmiami <tezosmiami@gmail.com>
2023-01-26 01:20:35 -0500

    hmmm. . .

--- src/components/objkt-post/index.js
+++ src/components/objkt-post/index.js
@@ -17,8 +17,10 @@ export const ObjktPost = (item) => {
 
     useEffect(() => {
       const getMetadataFile = async () => {
+        console.log('test' ,objkt)
         if (objkt.creator.metadata_file) {
           let meta = await axios.get('https:/ipfs.io/ipfs/' + objkt.creator.metadata_file.split('//')[1], {headers: {Accept: 'text/plain'}}).then(res => res.data)
+          console.log(meta)
           setLogo(meta.identicon) 
         }
       }
