9ad79b7133f0101e667ef0dc7c1116b729b5e364
tezosmiami <tezosmiami@gmail.com>
2023-06-01 01:01:12 -0400

    . . .

--- src/pages/config/index.js
+++ src/pages/config/index.js
@@ -271,15 +271,15 @@ export class Config extends Component {
             <Input
               name="lightning"
               onChange={this.handleChange}
-              placeholder="lightning (URL or Address)"
-              label="lightning (URL or Address)"
+              placeholder="Lightning (URL or Address)"
+              label="Lightning (URL or Address)"
               value={this.state.lightning}
             />
 
             <Button onClick={this.lightning_config}>
               <Purchase>Save      <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M5.52.359A.5.5 0 0 1 6 0h4a.5.5 0 0 1 .474.658L8.694 6H12.5a.5.5 0 0 1 .395.807l-7 9a.5.5 0 0 1-.873-.454L6.823 9.5H3.5a.5.5 0 0 1-.48-.641l2.5-8.5z"></path></svg></Purchase>
             </Button>
-            <p style={{ marginTop : '7.5px' }}>link your lightning (URL or address) to activate zaps</p>
+            <p style={{ marginTop : '7.5px' }}>link your Lightning (URL or Address) to activate zaps</p>
           </Padding>
         </Container>   
         <Container>
